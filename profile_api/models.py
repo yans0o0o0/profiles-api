ffc82620c5ff97cd0c1b547822264e6ab4e688ce
yandriel <yansiverio@gmail.com>
2020-09-27 18:26:25 -0400

    Adding Profile feed items to our models

--- profile_api/models.py
+++ profile_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
@@ -67,3 +68,14 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     def __str__(self):
         """ The string representation of our model """
         return self.email
+
+class ProfileFeedItem(models.Model):
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    create_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status_text
