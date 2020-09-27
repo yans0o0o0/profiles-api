2ad5156adb829aef3b0e1bf2cec1b498d500d67e
yandriel <yansiverio@gmail.com>
2020-09-27 17:42:54 -0400

    Adding Authentication token to users profile

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -8,5 +8,6 @@ router = DefaultRouter()
 router.register('profile',views.UserProfileViewSet)
 
 urlpatterns = [
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
