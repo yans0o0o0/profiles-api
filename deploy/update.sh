9d8aeebf9f23ce7ba8724b8e26d11d34a70b3235
yandriel <yansiverio@gmail.com>
2020-09-28 14:29:16 -0400

    Fixing supervisor

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-api'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+supervisorctl restart profile_api
 
 echo "DONE! :)"
